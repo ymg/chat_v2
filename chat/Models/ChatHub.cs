b6d38202c53e3e89527c1e6589f44bad53525e4b
Yasir <yasseral.g@gmail.com>
2013-05-30 23:48:37 +0100

    updated packages

--- chat/Models/ChatHub.cs
+++ chat/Models/ChatHub.cs
@@ -2,59 +2,48 @@
 using System.Linq;
 using System.Web;
 using System.Threading.Tasks;
-using System.Web.Script.Serialization;
 using Microsoft.AspNet.SignalR;
 
 namespace chat.Models
 {
     public class ChatHub : Hub
     {
-
-        private static readonly Dictionary<string, string> UserList = new Dictionary<string, string>();
+        // ReSharper disable FieldCanBeMadeReadOnly.Local
+        private static Dictionary<string, string> _userList = new Dictionary<string, string>();
+        // ReSharper restore FieldCanBeMadeReadOnly.Local
 
         public void AddText(string chatMessage)
         {
-            var validUser = UserList.SingleOrDefault(s => s.Key == Context.ConnectionId);
+            var validUser = _userList.SingleOrDefault(s => s.Key == Context.ConnectionId);
             var message = validUser.Value == string.Empty ? null : string.Format("<li><b>{0}</b>: {1} </li>", validUser.Value, HttpUtility.HtmlEncode(chatMessage));
             if (message != null)
                 Clients.All.retrieveInput(message);
         }
 
-        public string GetCurrentUsers()
-        {
-            var jsonObj = UserList.Count != -1
-                             ? new JavaScriptSerializer().Serialize(UserList)
-                             : string.Empty;
-            if (jsonObj != string.Empty)
-            {
-                Clients.All.currentUsers(jsonObj);
-                return jsonObj;
-            }
-            return null;
-        }
-
-        public void RemoveUser(string id)
-        {
-            var userInstance = UserList.SingleOrDefault(x => x.Key == id);
-            if (userInstance.Value == string.Empty) return;
-            UserList.Remove(id);
-        }
-
         public void AddInUserList(string nickName, string connectionId)
         {
-            UserList.Add(connectionId, nickName);
-            Clients.All.currentUsers(GetCurrentUsers());
+            _userList.Add(connectionId, HttpUtility.HtmlEncode(nickName));
+            OnConnected();
         }
 
         public override Task OnDisconnected()
         {
-            RemoveUser(Context.ConnectionId);
-            return Clients.All.currentUsers(GetCurrentUsers());
+            var userInstance = _userList.SingleOrDefault(x => x.Key == Context.ConnectionId);
+            if (userInstance.Value != string.Empty)
+                _userList.Remove(Context.ConnectionId);
+            OnConnected();
+            return base.OnDisconnected();
         }
 
         public override Task OnConnected()
         {
-            return Clients.All.currentUsers(GetCurrentUsers());
+            var jsonObj = _userList.Count != -1
+                             ? new System.Web.Script.Serialization.JavaScriptSerializer().Serialize(_userList)
+                             : string.Empty;
+            if (jsonObj != string.Empty)
+                Clients.All.currentUsers(jsonObj);
+
+            return base.OnConnected();
         }
     }
 }
